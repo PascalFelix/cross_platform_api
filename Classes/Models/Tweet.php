1053dd5c54e74c6a805d1cd3b7d7f02f794a24ed
Pascal Felix <pascalifix@gmail.com>
2020-10-16 04:16:09 +0200

    test for get tweet

---
+++ Classes/Models/Tweet.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Classes\Models;
+
+
+class Tweet extends BaseModel
+{
+    public function getTableName(): string
+    {
+        return 'tweets';
+    }
+
+    public function getContent(): string
+    {
+        return $this->Content;
+    }
+
+    public function getLikes(): int
+    {
+        return intval($this->Likes);
+    }
+
+    public function getRetweetCount(): int
+    {
+        return intval($this->Retweets);
+    }
+}
